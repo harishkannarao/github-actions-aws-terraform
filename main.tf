8b5fdb101f3eabf3de8d54b6469371218786a50b
Harish Kannarao <harishkannarao@harishs-mbp.home>
2019-12-25 20:08:50 +0000

    Updating s3 bucket name

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12.13"
-  backend "s3" {
-    bucket         = "github_actions_ci"
-    key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "github-actions-ci-locks"
-    encrypt        = true
-  }
+  #backend "s3" {
+  #  bucket         = "github-actions-ci"
+  #  key            = "terraform.tfstate"
+  #  region         = "eu-west-2"
+  #  dynamodb_table = "github-actions-ci-locks"
+  #  encrypt        = true
+  #}
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -19,6 +19,6 @@ provider "aws" {
 # Call the seed_module to build our ADO seed info
 module "bootstrap" {
   source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "github_actions_ci"
+  name_of_s3_bucket           = "github-actions-ci"
   dynamo_db_table_name        = "github-actions-ci-locks"
 }
