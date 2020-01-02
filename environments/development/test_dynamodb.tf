1e454023b5bc7e1ef0d546dffbae2dd4c8fb3c68
Harish Kannarao <harishkannarao@harishs-mbp.home>
2020-01-02 09:00:24 +0000

    Generating outputs

--- environments/development/test_dynamodb.tf
+++ environments/development/test_dynamodb.tf
@@ -1,4 +1,12 @@
-module "test-dynamodb" {
+module "test_dynamodb" {
   source                      = "../../modules/test-dynamodb"
   dynamo_db_table_name        = "test-dynamodb"
+}
+
+output "test-dynamodb-id" {
+  value = module.test_dynamodb.dynamo_db_table_id
+}
+
+output "test-dynamodb-arn" {
+  value = module.test_dynamodb.dynamo_db_table_arn
 }
--- modules/test-dynamodb/test-dynamodb-outputs.tf
+++ modules/test-dynamodb/test-dynamodb-outputs.tf
@@ -1,3 +1,7 @@
 output "dynamo_db_table_id" {
   value = aws_dynamodb_table.test-dynamodb.id
 }
+
+output "dynamo_db_table_arn" {
+  value = aws_dynamodb_table.test-dynamodb.arn
+}
