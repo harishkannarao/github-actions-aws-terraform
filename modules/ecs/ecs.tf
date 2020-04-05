c76fffa511aa7382a4d0b532ccf07b2ef3606d0b
harishkannarao <harish.gtec@gmail.com>
2020-04-05 20:05:24 +0530

    Redirecting http to https

--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -131,8 +131,13 @@ resource "aws_alb_listener" "docker_http_app" {
   depends_on        = ["aws_alb_target_group.docker_http_app_alb_target_group"]
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.docker_http_app_alb_target_group.arn}"
-    type             = "forward"
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
