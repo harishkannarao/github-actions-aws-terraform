02c6b8e5446e360c879a0aaf886a4db0dc990198
harishkannarao <harish.gtec@gmail.com>
2020-04-09 20:19:35 +0530

    Printing the vpc id using data as output

--- environments/development/ebs_iam_service_role.tf
+++ environments/development/ebs_iam_service_role.tf
@@ -1,3 +1,9 @@
 data "aws_iam_role" "ebs_iam_service_role" {
   name = "AWSServiceRoleForElasticBeanstalk"
 }
+
+data "aws_vpc" "test_vpc_data" {
+  tags = {
+    Name        = "${var.environment}-vpc"
+  }
+}
--- environments/development/outputs.tf
+++ environments/development/outputs.tf
@@ -2,6 +2,10 @@ output "elastic-beanstalk-service-role-arn" {
   value = "${data.aws_iam_role.ebs_iam_service_role.arn}"
 }
 
+output "test-vpc-data-id" {
+  value = "${data.aws_vpc.test_vpc_data.id}"
+}
+
 output "ecr-registry-id" {
   value = "${module.ecs.registry_id}"
 }
