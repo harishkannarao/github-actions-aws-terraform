262d8931a76e8e484f28615ccdab1152a96f2bc1
harishkannarao <harish.gtec@gmail.com>
2020-04-09 20:43:01 +0530

    Reverting changes to print vpc id using data

--- environments/development/ebs_iam_service_role.tf
+++ environments/development/ebs_iam_service_role.tf
@@ -1,9 +1,3 @@
 data "aws_iam_role" "ebs_iam_service_role" {
   name = "AWSServiceRoleForElasticBeanstalk"
 }
-
-data "aws_vpc" "test_vpc_data" {
-  tags = {
-    Name        = "${var.environment}-vpc"
-  }
-}
