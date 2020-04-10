2b572b17f50c16c53b8db4b3dfc299b87c05bc5b
harishkannarao <harish.gtec@gmail.com>
2020-04-10 14:01:35 +0530

    Doing init before destroy in CI

--- ci-terraform-destroy.sh
+++ ci-terraform-destroy.sh
@@ -6,4 +6,8 @@ set -e
 # Print the commands as it is executed. Useful for debugging
 set -x
 
+terraform init -input=false environments/$ENV_NAME
+
+terraform plan -input=false -var-file="variables/$ENV_NAME.tfvars" environments/$ENV_NAME
+
 terraform destroy -auto-approve -input=false -var-file="variables/$ENV_NAME.tfvars" environments/$ENV_NAME
