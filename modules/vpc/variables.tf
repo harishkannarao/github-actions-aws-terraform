2ca44800dbe81a2d27cf45ce330aabcf68ce10d2
Harish Kannarao <harishkannarao@harishs-mbp-2.home>
2020-09-27 23:54:38 +0100

    Removing unused variables from vpc module

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -6,21 +6,6 @@ variable "vpc_subnet_cidr" {
   description = "The subnet CIDR block of the vpc"
 }
 
-variable "public_subnets_cidr" {
-  type        = list(string)
-  description = "The CIDR block for the public subnet"
-}
-
-variable "private_subnets_cidr" {
-  type        = list(string)
-  description = "The CIDR block for the private subnet"
-}
-
 variable "environment" {
   description = "The environment"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "The az that the resources will be launched"
 }
