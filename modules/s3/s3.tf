2d93348699778930a506d00f1ee013e24501aeaa
harishkannarao <harishkannarao@users.noreply.github.com>
2024-03-22 23:39:11 +0000

    Fixing s3 bucket policy

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -32,7 +32,23 @@ resource "aws_s3_bucket_public_access_block" "www_bucket_public_access_block" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_ownership_controls" "www_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.www.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.www_bucket_public_access_block]
+}
+
+resource "aws_s3_bucket_acl" "www_bucket_act" {
+    bucket = aws_s3_bucket.www.id
+    acl    = "public-read"
+    depends_on = [aws_s3_bucket_ownership_controls.www_bucket_acl_ownership]
+}
+
+
 resource "aws_s3_bucket_policy" "www_bucket_policy" {
   bucket = aws_s3_bucket.www.id
   policy = data.template_file.www_s3_bucket_policy.rendered
+  depends_on = [aws_s3_bucket_public_access_block.www_bucket_public_access_block]
 }
