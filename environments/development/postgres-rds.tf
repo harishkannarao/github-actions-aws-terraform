9b1b899e429804cdcc0d5cfd9ec0bd932f5edeca
harishkannarao <harishkannarao@users.noreply.github.com>
2024-10-28 16:03:13 +0000

    Changing RDS instance type

--- environments/development/postgres-rds.tf
+++ environments/development/postgres-rds.tf
@@ -6,7 +6,7 @@ module "postgres-rds" {
   database_username = var.database_username
   subnet_ids        = module.vpc.private_subnets_id
   vpc_id            = module.vpc.vpc_id
-  instance_class    = "db.t3.micro"
+  instance_class    = "db.t4g.medium"
   multi_az          = var.database_multi_az
   security_groups_ids = [
     module.bastion.bastion_sg_id
