01f6c25b09fdf30a71ed50738030608e7916cce5
harishkannarao <harishkannarao@users.noreply.github.com>
2024-10-30 10:17:28 +0000

    Fixing CI

--- environments/development/terraform.tf
+++ environments/development/terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.7.5"
+  required_version = ">=1.7.5"
   backend "s3" {
     bucket         = "github-actions-ci"
     key            = "terraform-development.tfstate"
